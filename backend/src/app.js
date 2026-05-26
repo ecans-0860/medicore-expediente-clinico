98aa20062e226fe887566575090e1a9c3654df25
Marcous Lopez <mlopezg71@miumg.edu.gt>
2026-05-25 21:15:13 -0600

    chore: configuracion inicial del backend

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "MediCore API funcionando correctamente"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
