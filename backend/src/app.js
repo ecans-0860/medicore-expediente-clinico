7508f94d56076888725ee8be91c2f056d4d80833
Marcous Lopez <mlopezg71@miumg.edu.gt>
2026-05-25 22:08:00 -0600

    feat: configure prisma railway and express backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,37 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import prisma from "./config/prisma.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "MediCore API funcionando correctamente"
-    });
+app.get("/", async (req, res) => {
+
+    try {
+
+        await prisma.$connect();
+
+        res.json({
+            message: "MediCore API funcionando correctamente",
+            database: "Conectada correctamente a Railway"
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            message: "Error al conectar con la base de datos"
+        });
+
+    }
+
 });
 
 const PORT = process.env.PORT || 3000;
